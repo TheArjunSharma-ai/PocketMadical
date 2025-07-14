31c280d47d5c377450b053b745f8c9e3b4b3f2ef
Arjun Sharma <as506347@gmail.com>
2025-07-15 00:02:54 +0530

    second commit

--- obj/Debug/net8.0/PocketMedical.AssemblyInfo.cs
+++ obj/Debug/net8.0/PocketMedical.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocketMedical")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18100a71ade6dc909c96fc09ac4d26ba62f2a566")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocketMedical")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocketMedical")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
